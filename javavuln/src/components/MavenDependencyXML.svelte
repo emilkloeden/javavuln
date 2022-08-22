1ed557b29e2a973690ed40e20d6e7f654b1ca084
Emil Kloeden <emilkloeden@gmail.com>
2022-08-22 10:30:07 +0930

    FEAT: Added svelte-highlight dependency to allow for syntax highlighting.

--- javavuln/src/components/MavenDependencyXML.svelte
+++ javavuln/src/components/MavenDependencyXML.svelte
@@ -1,18 +1,26 @@
 <script>
+	import Highlight from 'svelte-highlight';
+	import xml from 'svelte-highlight/languages/xml';
+	import github from 'svelte-highlight/styles/github';
+
 	export let group_id;
 	export let artifact_id;
 	export let version;
-</script>
 
-<div>
-	<h2>Maven dependency XML</h2>
-	<pre><code
-			>{`<dependency>
+	const code = `<dependency>
 	<groupId>${group_id}</groupId>
 	<artifactId>${artifact_id}</artifactId>
 	<version>${version}</version>
-</dependency>`}</code
-		></pre>
+</dependency>`;
+</script>
+
+<svelte:head>
+	{@html github}
+</svelte:head>
+
+<div>
+	<h2>Maven dependency XML</h2>
+	<Highlight language={xml} {code} />
 </div>
 
 <style>
